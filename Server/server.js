c5e5156c5ed6724a8fca5d164d558f8dbe21978f
Karlex1 <sanchit959871@gmail.com>
2023-01-08 03:24:06 +0530

    learned about cors basic react and fixed git

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from 'cors';
+
 
 const app = express();
 const Port=4000;
@@ -9,12 +11,13 @@ const Port=4000;
 await mongoose.connect("mongodb+srv://sanchit:pass231020@mern-stack.tzfiyj1.mongodb.net/?retryWrites=true&w=majority").then(()=>{console.log("Database connected!");}).catch((err)=>{console.error(err);})
 
 
+app.use(cors);
 
 app.get(
     "/",(req,res)=>{res.send('Hello World!');}
 );
 app.listen(
     Port,()=>{
-        console.log("Server listen at Port: "+Port);
+        console.log("Server listen at : http://localhost:4000/ ");
     }
 )
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+
 
 function App() {
+  // state for form data.
+  const [form, setForm] = useState(
+    {
+      amount: '',
+      desc: '',
+      date: '',
+    }
+  );
+  //handleInput to collect data from input tag
+  const handleInput = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+
+  }
+  //HandleSubmit to submit form.
+async function handleSubmit  (event)  {
+    event.preventDefault();
+  const res = await fetch(
+    "http://localhost:4000/transaction",{
+      method:'POST',
+      body:form,
+    }
+   );
+console.log(res);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type='number'
+          name='amount'
+          onChange={handleInput}
+          value={form.amount}
+          placeholder='Enter transaction amount'
+        />
+<br/>
+        <input
+          name='desc'
+          onChange={handleInput}
+          value={form.desc}
+          placeholder='description'
+          type='text' />
+<br/>
+        <input
+          name='date'
+          placeholder='date'
+          type='date'
+          onChange={handleInput}
+          value={form.date} />
+<br/>
+        <button type='submit' >Submit</button>
+        <br/>
+      </form>
     </div>
   );
 }
