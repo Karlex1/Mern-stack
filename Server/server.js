017f4eb8beb9c26ec9c87077cdf191ef06da8c75
Karlex1 <sanchit959871@gmail.com>
2023-09-01 13:45:26 +0530

    changes

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ import path from 'path';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
-const path = require('path');
+// const path = require('path');
 
 app.use(cors());
 app.use(bodyParser.json());
